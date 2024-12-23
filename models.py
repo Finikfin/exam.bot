487a1016fed04ed6eaf17b725d610ea6317080c9
asdfrewqha <asdfrewqha@gmail.com>
2024-12-23 05:55:42 +0300

    adaptive

--- models.py
+++ models.py
@@ -1,27 +1,7 @@
 import random
+import config as c
 from datetime import datetime
-TICKET_IMAGES_COPY = [
-    "билеты/Билет№01.png",
-    "билеты/Билет№02.png",
-    "билеты/Билет№03.png",
-    "билеты/Билет№04.png",
-    "билеты/Билет№05.png",
-    "билеты/Билет№06.png",
-    "билеты/Билет№07.png",
-    "билеты/Билет№08.png",
-    "билеты/Билет№09.png",
-    "билеты/Билет№10.png",
-    "билеты/Билет№11.png",
-    "билеты/Билет№12.png",
-    "билеты/Билет№13.png",
-    "билеты/Билет№14.png",
-    "билеты/Билет№15.png",
-    "билеты/Билет№16.png",
-    "билеты/Билет№17.png",
-    "билеты/Билет№18.png",
-    "билеты/Билет№19.png",
-    "билеты/Билет№20.png"
-]
+TICKET_IMAGES_COPY = list(c.TICKET_IMAGES)
 class ExamTicket:
     def __init__(self):
         self.ticket_list = TICKET_IMAGES_COPY
