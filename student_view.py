a245a6c6f948a64280540b6523c4edbbcdd893ab
tuhlopuz1 <tuhlopuz1@gmail.com>
2024-12-23 04:52:43 +0300

    avoid double tickets

--- student_view.py
+++ student_view.py
@@ -10,23 +10,26 @@ async def start_command_student(message: types.Message):
     await message.answer("Привет! Я бот, который выдает билеты на экзамен. Напиши /ticket, чтобы получить билет.")
 
 async def get_ticket_command_student(message: types.Message):
-    exam_ticket = ExamTicket()
-    random_ticket = exam_ticket.get_random_ticket()
-    file = FSInputFile(random_ticket)
-    current_time = datetime.now()
-    # if current_time < datetime(2024, 12, 27, 10, 30):
-    #     await message.answer("Билеты будут доступны не ранее 10:30 27.12.")
-    #     return
+    if message.from_user.username not in list(BIND_TICKET_IMAGES.values()):
+        exam_ticket = ExamTicket()
+        random_ticket = exam_ticket.get_random_ticket()
+        file = FSInputFile(random_ticket)
+        current_time = datetime.now()
+        # if current_time < datetime(2024, 12, 27, 10, 30):
+        #     await message.answer("Билеты будут доступны не ранее 10:30 27.12.")
+        #     return
 
-    ticket = random_ticket
-    await message.answer_photo(photo=file, caption="Ваш билет. чтобы запросить консультацию, напиши /consultation, если ты готов сдавать, напиши /ready ")
-    
-    # Уведомление преподавателей
-    for teacher_id in TEACHER_IDS:
-        caption_text = f"@{message.from_user.username} запросил билет: {ticket}."
-        await message.bot.send_photo(chat_id=teacher_id, photo=file, caption=caption_text)
-    BIND_TICKET_IMAGES[ticket] = message.from_user.username
-    TICKET_IMAGES_COPY.remove(ticket)
+        ticket = random_ticket
+        await message.answer_photo(photo=file, caption="Ваш билет. чтобы запросить консультацию, напиши /consultation, если ты готов сдавать, напиши /ready ")
+        
+        # Уведомление преподавателей
+        for teacher_id in TEACHER_IDS:
+            caption_text = f"@{message.from_user.username} запросил билет: {ticket}."
+            await message.bot.send_photo(chat_id=teacher_id, photo=file, caption=caption_text)
+        BIND_TICKET_IMAGES[ticket] = message.from_user.username
+        TICKET_IMAGES_COPY.remove(ticket)
+    else:
+        await message.answer('Ты уже взял билет.')
 
         
 async def consultation_command_student(message: types.Message):
