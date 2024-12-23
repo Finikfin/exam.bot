20309f1d475216cebd1fccdd474d572498fb67f6
tuhlopuz1 <tuhlopuz1@gmail.com>
2024-12-23 05:45:51 +0300

    вроде финал

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Bot, Dispatcher, types, Router, F
 from aiogram.filters import Command
 from aiogram.types import FSInputFile
-from config import TOKEN
+from config import TOKEN, BIND_TICKET_IMAGES
 from role_manager import start_command, get_ticket_command, consultation_command, ready_command, get_tickets_command
 
 
@@ -40,12 +40,17 @@ dp.include_router(router)
 @router.callback_query(F.data.startswith('btn'))
 async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
     code = callback_query.data
+    code = code[3:]
     if len(str(code)) == 1:
         code = f'0{code}'
-    code = code[3:]
     caption_text = f'Вот ваш билет №{code}! '
     file = FSInputFile(f"билеты/Билет№{code}.png")
     await bot.send_photo(chat_id=callback_query.from_user.id, photo=file, caption=caption_text)
+    if f"билеты/Билет№{code}.png" in BIND_TICKET_IMAGES:
+        await bot.send_message(chat_id=callback_query.from_user.id, text=f'Этот билет взял @{BIND_TICKET_IMAGES[f"билеты/Билет№{code}.png"]}')
+    else:
+        await bot.send_message(chat_id=callback_query.from_user.id, text='Этот билет пока никто не взял')
+
 
 async def main():
     await dp.start_polling(bot)
