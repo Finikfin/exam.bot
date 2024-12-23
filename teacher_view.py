ef57df15e595ea4423c935024bac33fdf7426d9d
Ilya <finikgrib@gmail.com>
2024-12-23 04:10:15 +0300

    добавил вывод привязки билетов преподавателям

--- teacher_view.py
+++ teacher_view.py
@@ -7,6 +7,15 @@ from aiogram.utils import markdown as md
 from datetime import datetime
 from aiogram.types import FSInputFile
 from models import ExamTicket
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+inline_kb_full = InlineKeyboardMarkup()
+bl_1 = InlineKeyboardButton('билет 1', callback_data='btn1')
+bl_2 = InlineKeyboardButton('билет 2', callback_data='btn2')
+bl_3 = InlineKeyboardButton('билет 3', callback_data='btn3')
+inline_kb_full.add(bl_1, bl_2, bl_3)
+
 
 
 async def start_command_teacher(message: types.Message):
@@ -22,9 +31,6 @@ async def ready_command_teacher(message: types.Message):
     pass
 
 async def get_tickets_command_teacher(message: types.Message):
-<<<<<<< Updated upstream
-    await message.reply("Выберите билет", reply_markup=kb.inline_kb_full)
-=======
     tic_keys = BIND_TICKET_IMAGES.keys()
     all_tic = []
     for i in TICKET_IMAGES:
@@ -35,5 +41,4 @@ async def get_tickets_command_teacher(message: types.Message):
         all_tic.append(tic)
     await message.answer("Вот список всех билетов с привязкой к ученикам: ", all_tic)
     await message.reply("Выберите билет", reply_markup=kb.inline_kb1)
->>>>>>> Stashed changes
 
