a7c8ce62d3b5e2e0b01e0cb8511ea10c43a4d279
Finikfin <finikgrib@gmail.com>
2024-12-25 19:50:28 +0300

    собрал контейнер

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ dir = "./билеты"
 
 TOKEN = "7691139246:AAGVxWa_2QPiOPUoNjiqjgYRFe_l2QnBQFk"
 TEACHER_IDS = [6170560152, 245851335, 1362703815]  # Замените на реальные ID преподавателей
+STUDENT_IDS = [1648778328, 847867090, 1266213854, 114314156, 1751823515, 5064226866, 1309198139, 1254389035]
 TICKET_IMAGES = []
 for x in os.listdir(dir):
     TICKET_IMAGES.append(f'билеты/{x}')
